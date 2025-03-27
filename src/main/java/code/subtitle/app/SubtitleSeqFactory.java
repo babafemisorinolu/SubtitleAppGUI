5497740fafa4f2c361e7c000a8cd739e29129d00
Babafemi Sorinolu <babafemisorinolu@MacBookPro.houghton.edu>
2025-03-27 12:28:22 -0400

    initial commit

---
+++ src/main/java/code/subtitle/app/SubtitleSeqFactory.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code.subtitle.app;
+
+
+/**
+ *
+ * @author babafemisorinolu
+ */
+public class SubtitleSeqFactory {
+
+    	// Return an empty subtitles sequence 
+	public static SubtitleSeq getSubtitleSeq() {
+            return null;
+        } 
+ 
+    
+   // Load a subtitle sequence from an SRT file. If the file does not exist or // is corrupted (incorrect format), null is returned. 
+	public static SubtitleSeq loadSubtitleSeq(String fileName) { 
+	    return null;
+	} 
+
+}
